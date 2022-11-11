94a36ad1f48beaee83c14be5557c9696ddd2cf82
XIN-HUA-LI <christinelee1114@gmail.com>
2022-11-11 09:10:56 +0800

    1111"滑動"修改

--- js/top.js
+++ js/top.js
@@ -1,13 +1,22 @@
-// $("#top").click(function () {
-//   $("html,body").animate({ scrollTop: 0 })
-//   //   $("#totop").hide()
-//   return false
-// })
+// 網頁向下滑動 20px 出現"返回頂部"button
+window.onscroll = function () {
+  scrollFunction()
+}
 
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 1000) {
-    $("#totop").fadeIn()
+function scrollFunction() {
+  console.log(121)
+  if (
+    document.body.scrollTop > 800 ||
+    document.documentElement.scrollTop > 800
+  ) {
+    document.getElementById("totop").style.display = "block"
   } else {
-    $("#totop").fadeOut()
+    document.getElementById("totop").style.display = "none"
   }
-})
+}
+
+// 點擊按鈕，返回頂部
+function topFunction() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
